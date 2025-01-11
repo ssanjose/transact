2ac2c758ca5f9679792903bbe77603b9b57c5661
ssanjose <ssanjose@academic.rrc.ca>
2025-01-11 00:17:32 -0600

    add: delete transaction button

--- src/components/transaction/TransactionButtons.tsx
+++ src/components/transaction/TransactionButtons.tsx
@@ -5,11 +5,35 @@ import { Button } from '@/components/ui/button';
 import { DrawerDialog } from '@/components/shared/ResponsiveDrawerDialog';
 import { useDialog } from '@/hooks/use-dialog';
 import TransactionForm from './TransactionForm';
+import { TransactionService } from '@/services/transaction.service';
+import { Sleep } from '@/lib/utils';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog';
 
 interface TransactionButtonProps {
   button?: React.ReactNode;
-  accountId: number;
   dialogProps?: typeof useDialog;
+  title?: string;
+  description?: string;
+}
+
+interface OpenTransactionButtonProps extends TransactionButtonProps {
+  accountId: number;
+}
+
+interface DeleteTransactionButtonProps extends TransactionButtonProps {
+  id: number;
+  name?: string;
+  visible?: boolean;
 }
 
 /**
@@ -17,10 +41,10 @@ interface TransactionButtonProps {
  * @param {React.ReactNode} button - The button to render
  * @param {number} accountId - The account id
  */
-const OpenTransactionButton = ({ button, accountId, dialogProps }: TransactionButtonProps) => {
+const OpenTransactionButton = ({ button, accountId, dialogProps }: OpenTransactionButtonProps) => {
   const openTransactionDialog = dialogProps ? dialogProps() : useDialog();
   const buttonChildren = button || (
-    <Button size="icon" className="px-2 py-0 w-min text-xs leading-tight h-7 sm:p-2 sm:text-sm sm:leading-3 sm:h-9">
+    <Button size="icon" className="px-2 py-0 w-min text-xs leading-tight h-7 sm:p-2 sm:text-sm sm:leading-3 sm:h-8">
       <span>Add Transaction</span>
     </Button>
   );
@@ -37,4 +61,50 @@ const OpenTransactionButton = ({ button, accountId, dialogProps }: TransactionBu
   )
 };
 
-export { OpenTransactionButton };
+const DeleteTransactionButton = ({ id, button, title, description, name, dialogProps, visible }: DeleteTransactionButtonProps) => {
+  const deleteTransactionDialog = dialogProps ? dialogProps() : useDialog();
+  const buttonChildren = button || (
+    <Button variant="link" size="icon" className="p-none w-min">
+      <span>Delete Transaction</span>
+    </Button>
+  );
+
+  const handleDelete = async (id?: number) => {
+    const deleteTransaction = async () => {
+      if (id === -1)
+        return;
+
+      if (id!)
+        await TransactionService.deleteTransaction(id);
+      await Sleep(500);
+    }
+
+    deleteTransaction();
+    deleteTransactionDialog.dismiss();
+  }
+
+  return (
+    <AlertDialog {...deleteTransactionDialog.dialogProps}>
+      <AlertDialogTrigger asChild
+        className={visible ? "block" : "hidden"}
+      >{buttonChildren}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-lg font-semibold">
+            {title ? title : "Are you sure you want to delete this transaction?"}
+            {name ? `: ${name}` : ""}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {description ? description : "This action cannot be undone. This will permanently delete the transaction and all associated data such other transactions related to this transaction by frequency."}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => handleDelete(id)}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+};
+
+export { OpenTransactionButton, DeleteTransactionButton };
