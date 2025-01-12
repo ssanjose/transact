2122495dfb8b264eee10e7a526e828f29b724ca9
ssanjose <ssanjose@academic.rrc.ca>
2025-01-12 00:39:24 -0600

    refactor: analytics facade

---
+++ src/services/analytics/analytics.service.ts
@@ -0,0 +1,7 @@
+import { AccountAnalyticsService } from "@/services/analytics/account.analytics.service";
+import { TransactionAnalyticsService } from "./transaction.analytics.service";
+
+export const AnalyticsService = {
+  AccountAnalyticsService,
+  TransactionAnalyticsService,
+};
