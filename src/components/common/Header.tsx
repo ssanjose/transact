bc999c89450b9585114f4de8c6aa3e6632adb3df
ssanjose <ssanjose@academic.rrc.ca>
2025-01-14 06:47:36 -0600

    add: header for landing page

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,51 @@
 "use client";
 
-import React from "react"
+import React, { HTMLAttributes } from "react"
 import { ThemeModeToggle } from "@/components/theming/ThemeModeToggle";
-import { GitHubButton } from "@/components/common/SocialMediaIcons";
+import { cn } from "@/lib/utils";
+import { Inter } from 'next/font/google';
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
 
-const Header = () => {
+const inter = Inter({ subsets: ["latin"] });
+
+const Icons = () => {
   return (
-    <header className="sticky top-0 z-50 py-2 px-4 border-b flex justify-between items-center bg-background">
-      <span className="text-foreground text-xl md:text-2xl">
-        Finance Tracker App
-      </span>
-      <div>
-        <GitHubButton />
-        <ThemeModeToggle />
+    <div className="">
+      <ThemeModeToggle className="size-8 md:size-10" />
+    </div>
+  )
+}
+
+export const Links = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div className={cn("m-auto flex hidden sm:block", className)} style={{ width: "100%", maxWidth: "20rem", height: "1.5rem" }}>
+      <div className="flex gap-2 items-center w-fit absolute m-auto right-0 left-0 justify-center item-center">
+        <ul className="flex flex-row gap-6">
+          {siteConfig.navLinks?.map(({ href, text }) => (
+            <li key={href}>
+              <Link href={href} className="text-md text-inherit opacity-70 hover:opacity-100 hover:underline">
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
+    </div>
+  )
+}
+
+const Header = ({ className }: HTMLAttributes<HTMLElement>) => {
+  return (
+    <header className={cn(`py-2 flex justify-between items-center bg-background`, className, inter.className)}
+    >
+      <Link href="/">
+        <h1 className="text-foreground text-xl md:text-2xl font-bold">
+          {siteConfig.name}
+        </h1>
+      </Link>
+      <Links />
+      <Icons />
     </header>
   )
 }
