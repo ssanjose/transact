5c55e7d4bff8f8b101c32f73196fdc46adca8893
ssanjose <ssanjose@academic.rrc.ca>
2025-01-14 06:49:41 -0600

    add: initial peek of features section

---
+++ src/components/landing/Snapshot.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import React from 'react';
+import { cn } from '@/lib/utils';
+import LandingPageHeading from '@/components/landing/Heading';
+import ImageFrame from '@/components/common/ImageFrame';
+
+const images = [
+  {
+    src: "/transact_overview1.jpg",
+    alt: "Transact Overview",
+    width: "1000",
+    height: "800",
+    className: "",
+  },
+  {
+    src: "/transact_landing.jpg",
+    alt: "Transact Landing",
+    width: "1000",
+    height: "800",
+    className: "",
+  },
+  {
+    src: "/transact_overview.jpg",
+    alt: "Transact Landing",
+    width: "1000",
+    height: "800",
+    className: "",
+  },
+  {
+    src: "/transact_overview.jpg",
+    alt: "Transact Landing",
+    width: "1000",
+    height: "800",
+    className: "",
+  },
+]
+
+const Images = () => {
+  return (
+    <div className={cn(`grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8`)}>
+      {images.map((image, i) => (
+        <ImageFrame key={i}
+          src={image.src}
+          width={image.width}
+          height={image.height}
+          alt={image.alt}
+          className={image.className}
+          containerClassName="mt-[2rem] mx-0"
+        />
+      ))}
+    </div>
+  )
+}
+
+const Snapshot = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <section id="about" className={cn(`text-center flex flex-col`, className)}>
+      <LandingPageHeading
+        className="pb-4 min-h-[5rem]"
+        mainHeading="Want more?"
+        subHeading="Take a peek of what's to come"
+      />
+      <Images />
+    </section>
+  )
+}
+
+export default Snapshot;
