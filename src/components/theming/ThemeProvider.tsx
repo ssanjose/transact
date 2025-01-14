272c1daac907c370ffe2898ae13ff2e8890fef3d
ssanjose <ssanjose@academic.rrc.ca>
2025-01-14 06:53:35 -0600

    refactor: theme provider to chose between static or dynamic provider based on env variable node_env

--- src/components/theming/ThemeProvider.tsx
+++ src/components/theming/ThemeProvider.tsx
@@ -2,15 +2,18 @@
 
 import dynamic from 'next/dynamic';
 import React from 'react';
+import { ThemeProvider as StaticProvider } from 'next-themes';
 
 // dynamic import
 // see https://github.com/shadcn-ui/ui/issues/5552#issuecomment-2435053678
-const NextThemeProvider = dynamic(
+const DynProvider = dynamic(
   () => import('next-themes').then((e) => e.ThemeProvider),
   { ssr: false }
 );
 
-const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemeProvider>) => {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof StaticProvider>) {
+  const NextThemeProvider = process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? StaticProvider : DynProvider;
+
   return (
     <NextThemeProvider {...props}>
       {children}
