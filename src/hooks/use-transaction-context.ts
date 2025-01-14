72301bf35cd3c106ce93e7756a8889aa1e779272
ssanjose <ssanjose@academic.rrc.ca>
2025-01-13 18:02:33 -0600

    add: transaction context hook

---
+++ src/hooks/use-transaction-context.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Transaction } from '@/lib/db/db.model';
+
+const TransactionContext = React.createContext<Transaction[] | null | undefined>(null);
+
+export const useTransactionContext = () => {
+  const transactions = React.useContext(TransactionContext);
+
+  if (transactions === undefined) {
+    throw new Error('useTransactionContext must be used within a TransactionProvider');
+  }
+
+  return transactions;
+}
