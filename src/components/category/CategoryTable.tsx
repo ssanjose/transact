b83c802d76252d40660e594903c6ab9c917c4ee6
ssanjose <ssanjose@academic.rrc.ca>
2025-02-01 00:44:38 -0600

    add: actions to edit or delete categories in CategoryTable

--- src/components/category/CategoryTable.tsx
+++ src/components/category/CategoryTable.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
 import {
   ColumnDef,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 import { Category } from "@/lib/db/db.model"
-import DataTable from "../data-table/DataTable"
+import DataTable from "@/components/data-table/DataTable"
 import { useTransactionContext } from "@/hooks/use-transaction-context"
+
+import { DeleteCategoryButton, EditCategoryButton } from "@/components/category/CategoryButtons"
+import { CategoryService } from "@/services/category.service"
+import { useDialog } from "@/hooks/use-dialog"
+import { Button } from "@/components/ui/button"
+import { MoreHorizontal } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 interface CategoryTableProps {
   categories: Category[]
@@ -16,6 +29,22 @@ interface CategoryTableProps {
 
 const CategoryTable = ({ categories }: CategoryTableProps) => {
   const transactions = useTransactionContext();
+  const [selectedRowForAction, setSelectedRowForAction] = React.useState<number | undefined>()
+  const [rowData, setRowData] = React.useState<Category>({} as Category)
+
+  const deleteDialog = useDialog()
+  const editDialog = useDialog()
+
+  useEffect(() => {
+    if (selectedRowForAction === -1 || !selectedRowForAction) return;
+    let isMounted = true;
+    (async () => {
+      const data = await CategoryService.getCategory(selectedRowForAction);
+      if (isMounted) setRowData(data as Category);
+    })();
+
+    return () => { isMounted = false };
+  }, [selectedRowForAction])
 
   const columns: ColumnDef<Category>[] = [
     {
@@ -41,19 +70,77 @@ const CategoryTable = ({ categories }: CategoryTableProps) => {
         return count
       }
     },
+    {
+      id: "actions",
+      cell: ({ row, table }) => {
+        const category = row.original
+
+        return (
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                className="size-8 p-0"
+                onClick={() => table.options.meta?.setId(category.id ?? -1)}
+              >
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => {
+                  table.options.meta?.editDialogTrigger()
+                  table.options.meta?.removeId()
+                }}
+              >
+                Edit Category
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => {
+                  table.options.meta?.deleteDialogTrigger()
+                }}
+              >
+                Delete Category
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
+    },
   ]
 
   const table = useReactTable({
     data: categories,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    meta: {
+      setId: (id: number) => setSelectedRowForAction(id),
+      removeId: () => setSelectedRowForAction(undefined),
+      deleteDialogTrigger: () => deleteDialog.trigger(),
+      editDialogTrigger: () => editDialog.trigger(),
+    }
   })
 
   return (
-    <DataTable
-      columns={columns}
-      table={table}
-    />
+    <>
+      <DataTable
+        columns={columns}
+        table={table}
+      />
+      <EditCategoryButton
+        existingCategory={rowData}
+        dialogProps={() => editDialog}
+        visible={false}
+        title={`Edit ${rowData?.name}`}
+      />
+      <DeleteCategoryButton
+        id={rowData?.id || -1}
+        dialogProps={() => deleteDialog}
+        visible={false}
+      />
+    </>
   )
 }
 
