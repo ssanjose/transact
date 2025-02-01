e89111ea6dc13cc779e80172310f945188898686
ssanjose <ssanjose@academic.rrc.ca>
2025-02-01 01:32:43 -0600

    refactor: transcation details screen and sizing

--- src/components/transaction/TransactionDetails.tsx
+++ src/components/transaction/TransactionDetails.tsx
@@ -2,17 +2,28 @@
 
 import React, { useEffect, useState } from 'react';
 import { TransactionService } from '@/services/transaction.service';
-import { Transaction } from '@/lib/db/db.model';
+import { Frequency, Transaction, TransactionType } from '@/lib/db/db.model';
 import { cn } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 
 import { Inter } from 'next/font/google';
+import { format } from 'date-fns';
+import { formatCurrency } from '../../lib/format/formatCurrency';
 const inter = Inter({ subsets: ["latin"] });
 
-const DetailsImage = ({ className }: { className?: string }) => {
+const DetailsImage = ({ className, transaction }: { className?: string, transaction: Transaction }) => {
   return (
     <div className={cn("overflow-hidden relative", className)}>
-      <h2 className="absolute z-10 bottom-0 left-0 text-2xl text-black font-bold px-2 p-1">Transaction Details</h2>
+      <div className="absolute z-10 bottom-0 left-0 text-black px-2 p-1">
+        <h2 className="text-xl font-bold truncate">
+          {transaction.name} ({transaction.id})
+        </h2>
+        <p className="text-sm font-light">
+          {transaction.status === "pending" ? "Scheduled" : "Completed"} on {
+            format(transaction.date, "MMM dd,yyyy, hh:mm a")
+          }
+        </p>
+      </div>
       <img
         src="/pexels-voitkevich-6214459.jpg"
         alt="transaction"
@@ -46,7 +57,7 @@ const TransactionDetails = ({ id, className }: { id: number, className?: string 
     <div className={cn("h-fit", className, inter.className)}>
       {loading ?
         (transaction &&
-          <div className="flex justify-center">
+          <div className="flex justify-center items-center h-full">
             <Loader2 className="animate-spin h-12 w-12" />
           </div>
         )
@@ -54,13 +65,12 @@ const TransactionDetails = ({ id, className }: { id: number, className?: string 
         <>
           {transaction ? (
             <>
-              <DetailsImage className="w-full h-1/5" />
-              <div className="h-4/5 p-2 bg-card-overview text-foreground">
-                <h3 className="text-md text-normal truncate mr-10">{transaction.name}</h3>
-                <p className="text-sm text-light">Amount: {transaction.amount}</p>
-                <p className="text-sm text-light">Date: {transaction.date.toDateString()}</p>
-                <p className="text-sm text-light">Type: {transaction.type}</p>
-                <p className="text-sm text-light">Frequency: {transaction.frequency}</p>
+              <DetailsImage className="w-full h-1/2" transaction={transaction} />
+              <div className="h-1/2 p-2 bg-card-overview/30 text-foreground">
+                <DetailsText tL="Category: " tR={transaction.categoryId?.toString() || "None"} />
+                <DetailsText tL="Type: " tR={TransactionType[transaction.type]} />
+                <DetailsText tL="Frequency: " tR={Frequency[transaction.frequency]} />
+                <DetailsText tL="Amount:" tR={formatCurrency(transaction.amount)} />
               </div>
             </>
           ) : (
@@ -69,6 +79,15 @@ const TransactionDetails = ({ id, className }: { id: number, className?: string 
         </>
       }
     </div>
+  )
+}
+
+const DetailsText = ({ className, tL, tR }: { className?: string, tL: string, tR: string }) => {
+  return (
+    <p className={cn("flex justify-between items-center text-sm", className)}>
+      <span className="font-light">{tL}</span>
+      <span className="font-semibold">{tR}</span>
+    </p>
   )
 }
 
