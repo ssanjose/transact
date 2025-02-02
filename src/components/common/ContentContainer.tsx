c573dba09a053b18ff132f981538d4d310a0ceb1
ssanjose <ssanjose@academic.rrc.ca>
2025-02-01 23:33:46 -0600

    add: id 'top' to container

--- src/components/common/ContentContainer.tsx
+++ src/components/common/ContentContainer.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 
 const ContentContainer = ({ children, className }: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={cn(`w-full p-0 px-2 sm:pb-10 sm:px-4 relative font-[family-name:var(--font-geist-sans)]`, className)}>
+    <div id="top" className={cn(`w-full p-0 px-2 sm:pb-10 sm:px-4 relative font-[family-name:var(--font-geist-sans)]`, className)}>
       {children}
     </div>
   );
