3230246db1f79963c97fb1653176b8023710d9e8
ssanjose <ssanjose@academic.rrc.ca>
2025-02-02 04:02:02 -0600

    refactor: dashboard to use category context

--- src/app/(application)/analysis/dashboard/page.tsx
+++ src/app/(application)/analysis/dashboard/page.tsx
@@ -13,6 +13,9 @@ import { TransactionService } from '@/services/transaction.service';
 import { ExpenseTransactionChart, IncomeTransactionChart } from '@/components/overview/TransactionChartSummary';
 import { DateRange } from 'react-day-picker';
 import { TransactionContext } from '@/hooks/use-transaction-context';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { CategoryService } from '@/services/category.service';
+import { CategoryContext } from '@/hooks/use-category-context';
 
 const Page = () => {
   const [date, setDate] = React.useState<DateRange>({
@@ -20,6 +23,7 @@ const Page = () => {
     to: new Date(),
   });
   const [transactions, setTransactions] = React.useState<Transaction[] | null | undefined>(null);
+  const categories = useLiveQuery(() => CategoryService.getAllCategories());
 
   useEffect(() => {
     let isMounted = true;
@@ -32,30 +36,32 @@ const Page = () => {
             new Date(date.from!.getFullYear(), date.from!.getMonth(), date.from!.getDate(), 23, 59, 59, 999),
         sorted: true
       });
-      setTransactions(data);
+      if (isMounted) setTransactions(data);
     })();
 
     return () => { isMounted = false; }
   }, [date])
 
   return (
-    <TransactionContext.Provider value={transactions}>
-      <ContentContainer className="flex flex-col gap-2 min-h-screen pt-4">
-        <div className="block lg:grid lg:grid-cols-3 pb-0 px-2 sm:px-2 gap-4 w-full">
-          <div className="w-full lg:col-span-2 relative">
-            <DateRangePicker className="w-fit block lg:absolute top-0 right-0"
-              date={date!}
-              setDate={setDate}
-            />
-            <div className="w-full block lg:grid lg:grid-cols-2 gap-2 mt-10">
-              <IncomeTransactionChart className="w-full" />
-              <ExpenseTransactionChart className="w-full" />
+    <CategoryContext.Provider value={categories || []}>
+      <TransactionContext.Provider value={transactions}>
+        <ContentContainer className="flex flex-col gap-2 min-h-screen pt-4">
+          <div className="block lg:grid lg:grid-cols-3 pb-0 px-2 sm:px-2 gap-4 w-full">
+            <div className="w-full lg:col-span-2 relative">
+              <DateRangePicker className="w-fit block lg:absolute top-0 right-0"
+                date={date!}
+                setDate={setDate}
+              />
+              <div className="w-full block lg:grid lg:grid-cols-2 gap-2 mt-10">
+                <IncomeTransactionChart className="w-full" />
+                <ExpenseTransactionChart className="w-full" />
+              </div>
             </div>
+            <TransactionsOverview className="w-full lg:col-span-1" />
           </div>
-          <TransactionsOverview className="w-full lg:col-span-1" />
-        </div>
-      </ContentContainer>
-    </TransactionContext.Provider>
+        </ContentContainer>
+      </TransactionContext.Provider>
+    </CategoryContext.Provider>
   );
 }
 
