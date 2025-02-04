c596f0916cdb2c86bdcfe190e9745e008b15dc93
ssanjose <ssanjose@academic.rrc.ca>
2025-02-04 17:05:26 -0600

    remove: automated build of service worker

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,13 +1,28 @@
 /// <reference lib="webworker" />
 
+declare let self: ServiceWorkerGlobalScope;
+
+import { AccountService } from "./services/account.service";
+
 self.addEventListener('install', e => {
-  console.log('Service Worker Installed')
+  e.waitUntil(
+    Promise.resolve().then(() => {
+      console.log('Service Worker Installed');
+    })
+  );
 });
 self.addEventListener('activate', e => {
-  console.log('Service Worker Activated')
+  e.waitUntil(
+    Promise.resolve().then(() => {
+      console.log('Service Worker Activated');
+      return AccountService.applyTransactionsToAccount();
+    })
+  );
 });
 self.addEventListener('message', e => {
-  let data = new Array(e.data.split(" "))
-  self.postMessage(data)
-  console.log("Message from parent: " + e.data)
+  const data = e.data.split(" ");
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.postMessage(data));
+  });
+  console.log("Message from parent:", e.data);
 });
