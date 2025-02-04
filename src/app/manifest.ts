604b91d1498b097c89262cc69443120982627391
ssanjose <ssanjose@academic.rrc.ca>
2025-02-04 05:30:51 -0600

    create: manifest for pwa

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+import { siteConfig } from '../config/site'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteConfig.name,
+    short_name: siteConfig.name,
+    description: siteConfig.description,
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000000',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
