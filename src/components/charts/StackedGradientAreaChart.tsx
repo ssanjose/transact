812d789c64f306977478daf31cfcab769af5a472
ssanjose <ssanjose@academic.rrc.ca>
2025-02-04 14:02:13 -0600

    use: loading state

--- src/components/charts/StackedGradientAreaChart.tsx
+++ src/components/charts/StackedGradientAreaChart.tsx
@@ -72,6 +72,64 @@ export const StackedGradientAreaChart = ({
     ))
     , [config]);
 
+  if (!loading) {
+    return (
+      <Card className={className} {...props}>
+        <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+          <div className="grid flex-1 gap-1 text-center sm:text-left">
+            <CardTitle>{title}</CardTitle>
+            {description && <CardDescription>{description}</CardDescription>}
+          </div>
+        </CardHeader>
+        <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+          <ChartContainer
+            config={config}
+            className="aspect-auto h-[250px] w-full"
+          >
+            <AreaChart data={data}>
+              <defs>
+                {gradientDefinitions}
+              </defs>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                minTickGap={32}
+                tickFormatter={(value) => {
+                  const date = new Date(value)
+                  return date.toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                  })
+                }}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={
+                  <ChartTooltipContent
+                    labelFormatter={(value) => {
+                      return new Date(value).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                      })
+                    }}
+                    indicator="dot"
+                  />
+                }
+              />
+              {areaComponents}
+              {showLegend && <ChartLegend
+                className="flex flex-wrap justify-center"
+                content={<ChartLegendContent />} />}
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    )
+  }
+
   return (
     <Card className={className} {...props}>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
