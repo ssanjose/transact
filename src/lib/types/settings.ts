bd4ec0811ef3f0307b0782450efd9f8566cb7345
ssanjose <ssanjose@academic.rrc.ca>
2025-02-03 23:32:23 -0600

    add: new settings and const min and max for number inputs

--- src/lib/types/settings.ts
+++ src/lib/types/settings.ts
@@ -1,13 +1,28 @@
+export const NUMBER_INPUT_MIN = 1;
+export const NUMBER_INPUT_MAX = 6;
+
 export interface AppSettings {
+  appUpdate: boolean;
+  transactionUpdates: boolean;
+
   recentTransactionLimit: number;
   upcomingTransactionLimit: number;
+
+  continuousTransactions: boolean;
+
   defaultCurrency: string;
   dateFormat: string;
 }
 
 export const DEFAULT_SETTINGS: AppSettings = {
+  appUpdate: true,
+  transactionUpdates: true,
+
   recentTransactionLimit: 3,
   upcomingTransactionLimit: 3,
+
+  continuousTransactions: false,
+
   defaultCurrency: 'USD',
   dateFormat: 'MMM dd, YYYY'
 };
