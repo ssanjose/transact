dc91a35b82e73ae46941d10b191d4c2c26dfb208
ssanjose <ssanjose@academic.rrc.ca>
2025-02-05 20:11:15 -0600

    refactor: message event listener to handle different message types

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -2,19 +2,23 @@
 
 declare let self: ServiceWorkerGlobalScope;
 
+import { SWMessageType } from "../lib/types/sw";
 import { AccountService } from "../services/account.service";
 
 self.addEventListener('install', e => {
   console.log('Service Worker Installed', e);
 });
-self.addEventListener('activate', e => {
+self.addEventListener('activate', async (e) => {
   console.log('Service Worker Activated', e);
-  return AccountService.applyTransactionsToAccount();
 });
-self.addEventListener('message', e => {
-  const data = e.data.split(" ");
-  self.clients.matchAll().then(clients => {
-    clients.forEach(client => client.postMessage(data));
-  });
-  console.log("Message from parent:", e.data);
+self.addEventListener('message', async (event: ExtendableMessageEvent) => {
+  const message: SWMessageType = event.data;
+  switch (message.action) {
+    case 'COMMIT_TRANSACTIONS':
+      await AccountService.applyTransactionsToAccount();
+      break;
+    // ... other cases
+    default:
+      break;
+  }
 });
