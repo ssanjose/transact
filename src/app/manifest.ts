ca414f607afc51c27f01d1312f31cf7f1db64ac5
ssanjose <ssanjose@academic.rrc.ca>
2025-02-05 23:21:15 -0600

    update: proper manifest start_url, icons, screenshots, and purpose

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,23 +6,45 @@ export default function manifest(): MetadataRoute.Manifest {
     name: siteConfig.name,
     short_name: siteConfig.name,
     description: siteConfig.description,
-    start_url: '/',
+    start_url: '/overview',
     display: 'standalone',
     background_color: '#000000',
     theme_color: '#000000',
     icons: [
       {
+        src: '/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
         src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable'
+        purpose: 'any'
       },
       {
         src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable'
+        purpose: 'any'
       },
     ],
+    screenshots: [
+      {
+        src: '/screenshot-540x810.jpg',
+        sizes: '540x810',
+        type: 'image/png',
+        form_factor: 'narrow',
+        label: 'Transact App'
+      },
+      {
+        src: '/screenshot-1230x615.jpg',
+        sizes: '1230x615',
+        type: 'image/png',
+        form_factor: 'wide',
+        label: 'Transact App'
+      },
+    ]
   }
 }
