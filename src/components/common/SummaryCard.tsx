e21e8ef25bf4ec5bcc2b3b758fb792b48609475a
ssanjose <ssanjose@academic.rrc.ca>
2025-02-11 05:08:47 -0600

    style: add responsiveness to summary cards

--- src/components/common/SummaryCard.tsx
+++ src/components/common/SummaryCard.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
+import { cn } from '@/lib/utils';
 
 const SummaryCard = ({
   title,
@@ -9,13 +10,13 @@ const SummaryCard = ({
   description,
   className }: { title: string, subHeading: string, description: string, className: string }) => {
   return (
-    <Card className={className}>
+    <Card className={cn("flex flex-col gap-2", className)}>
       <CardHeader className="space-y-0 p-4 pb-0 lg:p-6 lg:pb-0">
         <CardTitle className="leading-none tracking-tight text-sm">{title}</CardTitle>
-        <CardDescription className="text-sm tracking-tight">{subHeading}</CardDescription>
+        <CardDescription className="text-xs md:text-sm tracking-tight">{subHeading}</CardDescription>
       </CardHeader>
-      <CardContent className="flex items-center gap-2 p-4 pt-2 lg:p-6 lg:pt-2">
-        <span className="text-lg font-semibold">
+      <CardContent className="flex items-center gap-2 p-4 pt-0 lg:p-6 lg:pt-2">
+        <span className="text-base md:text-lg font-semibold">
           {description}
         </span>
       </CardContent>
