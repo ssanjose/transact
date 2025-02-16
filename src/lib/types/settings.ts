206170f3fec3c7f68dbb2bc0bac11c24e98d153a
ssanjose <ssanjose@academic.rrc.ca>
2025-02-16 01:07:43 -0600

    refactor: AppSettings.currencyFormat to be typeof Currencies[number], and set default currencyFormat to USD and transactionUpdates to false

--- src/lib/types/settings.ts
+++ src/lib/types/settings.ts
@@ -1,3 +1,5 @@
+import { Currencies } from "@/config/currency";
+
 export const NUMBER_INPUT_MIN = 1;
 export const NUMBER_INPUT_MAX = 6;
 
@@ -10,19 +12,20 @@ export interface AppSettings {
 
   recurringTransactions: boolean;
 
-  currencyFormat: string;
+  currencyFormat: typeof Currencies[number];
   dateFormat: string;
 }
 
 export const DEFAULT_SETTINGS: AppSettings = {
   appUpdates: true,
-  transactionUpdates: true,
+  transactionUpdates: false,
 
   recentTransactionLimit: 3,
   upcomingTransactionLimit: 3,
 
   recurringTransactions: false,
 
-  currencyFormat: 'USD',
+  // Default currency format - USD
+  currencyFormat: Currencies[0],
   dateFormat: 'MMMM d, yyyy'
 };
