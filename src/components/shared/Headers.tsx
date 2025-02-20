853c21f768ce1e46321f48f7ffe02e4a8647609f
ssanjose <ssanjose@academic.rrc.ca>
2025-02-19 22:24:00 -0600

    create: Title header as h1 comp and refactor SectionTitle into h2 component

--- src/components/shared/Headers.tsx
+++ src/components/shared/Headers.tsx
@@ -1,14 +1,22 @@
 'use client';
 
 import React from 'react';
-import { cn } from '../../lib/utils';
+import { cn } from '@/lib/utils';
+
+const Title = ({ className, children }: { className?: string, children?: React.ReactNode }) => {
+  return (
+    <h1 className={cn("text-2xl md:text-3xl font-bold", className)}>
+      {children}
+    </h1>
+  )
+}
 
 const SectionTitle = ({ className, children }: { className?: string, children?: React.ReactNode }) => {
   return (
-    <h2 className={cn("text-1xl mb-2", className)}>
+    <h2 className={cn("text-xl md:text-2xl font-semibold", className)}>
       {children}
     </h2>
   )
 }
 
-export { SectionTitle };
+export { Title, SectionTitle };
